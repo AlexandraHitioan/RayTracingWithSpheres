52b0479057625bd32766e95cf57f2499a803e8d5
AlexandraHitioan <alexandra.hitioan22@gmail.com>
2023-11-01 19:26:09 +0200

    what works so far

--- Lab1/Sphere.cs
+++ Lab1/Sphere.cs
@@ -15,8 +15,31 @@ namespace rt
 
         public override Intersection GetIntersection(Line line, double minDist, double maxDist)
         {
-            // ADD CODE HERE: Calculate the intersection between the given line and this sphere
-            return new Intersection();
+             //ADD CODE HERE: Calculate the intersection between the given line and this sphere
+            var a = line.Dx * line.Dx;
+            var b = (line.Dx * line.X0) * 2;
+            b -= (line.Dx * Center) * 2;
+            var c = (line.X0 * line.X0) + (Center * Center) - (Radius * Radius) - (line.X0 * Center) * 2;
+            var discr = (b * b) - (a * c * 4.0);
+            if (discr < 0.001)
+                return new Intersection(false, false, this, line, 0);
+            var t1 = -b - Math.Sqrt(discr);
+            var t2 = -b + Math.Sqrt(discr);
+            t1 /= 2.0 * a;
+            t2 /= 2.0 * a;
+            var validT1 = t1 >= minDist && t1 <= maxDist;
+           var validT2 = t2 >= minDist && t2 <= maxDist;
+            if (!validT1 && !validT2)
+                return new Intersection(false, false, this, line, 0);
+            if (validT1 && !validT2)
+                return new Intersection(true, true, this, line, t1); 
+            if (!validT1)
+                return new Intersection(true, true, this, line, t2);
+            var mn = t1;
+            if (t2 < mn)
+                mn = t2;
+            return new Intersection(true, true, this, line, mn);
+           // return new Intersection();
         }
 
         public override Vector Normal(Vector v)
